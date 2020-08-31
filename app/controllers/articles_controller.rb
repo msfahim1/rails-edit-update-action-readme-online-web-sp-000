e93d37703ef01c1fd74977b8e1aafaad66764adf
Michael Fahim <michael_fahim@yahoo.com>
2020-08-31 12:21:41 +0000

    Automatically backed up by Learn

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -26,10 +26,7 @@ class ArticlesController < ApplicationController
 end
 
 def update
-  def update
-    @article = Article.find(params[:id])
-    @article.update(title: params[:article][:title], description: params[:article][:description])
-    redirect_to article_path(@article)
-  end
+  raise params.inspect
+
 end
 end
